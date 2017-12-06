3bd4883e5c81547b8ffa7edcbec0d5e597dbe7e7
Tony Apuzzo <Tony.Apuzzo@flatironsjouve.com>
2017-12-05 19:02:28 -0700

    Cleanup

--- Profile.d/fzf.psm1
+++ Profile.d/fzf.psm1
@@ -3,10 +3,10 @@
 # Alias fzf to workaround https://github.com/junegunn/fzf/issues/963
 function fzf() {
   try {
-    $fzf = Get-Command -CommandType Application fzf -ErrorAction Stop
+    $fzf = Get-Command -CommandType Application -ErrorAction Stop fzf
     if (Test-Path Env:\TERM) {
       $saveTERM = $Env:TERM
-      $Env:TERM = ""
+      $Env:TERM = ''
     }
     & $fzf @Args
   }
