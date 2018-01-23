9449585e14fe043652fda51ba24c7dac26e617de
Tony Apuzzo <Tony.Apuzzo@flatironsjouve.com>
2018-01-23 16:45:07 -0700

    Make vmSwitchUtils robust in the face of 'Default Switch'

--- Profile.d/vmSwitchUtils.ps1
+++ Profile.d/vmSwitchUtils.ps1
@@ -1,23 +1,43 @@
-# VMSwitch Utils to make moving between networks easier
+# VMSwitch Utils to make moving between networks easier when using Vagrant and Hyper-V
 
 function create-vmswitch() {
-  if (Get-VMSwitch -Name ExternalSwitch) {
-    echo "ExternalSwitch Already Exists"
-    Get-VMSwitch -Name ExternalSwitch
+
+# Network Bridge to Use (Defaults to "ExternalSwitch")
+  if (-not (Test-Path env:VAGRANT_NETWORK_BRIDGE)) { $env:VAGRANT_NETWORK_BRIDGE = 'ExternalSwitch' }
+  
+  if ($env:VAGRANT_NETWORK_BRIDGE -eq 'Default Switch') {
+    echo "You're using the 'Default Switch' and thus no changes can be made."
   } else {
-    if (-not (Test-Path env:WIRED_NETADAPTERNAME)) { $env:WIRED_NETADAPTERNAME = 'Ethernet' }
-    New-VMSwitch -SwitchName ExternalSwitch -NetAdapterName $env:WIRED_NETADAPTERNAME -AllowManagementOS $true
+    if (Get-VMSwitch -Name $env:VAGRANT_NETWORK_BRIDGE) {
+      echo "$env:VAGRANT_NETWORK_BRIDGE Already Exists"
+      Get-VMSwitch -Name  $env:VAGRANT_NETWORK_BRIDGE
+    } else {
+      if (-not (Test-Path env:WIRED_NETADAPTERNAME)) { $env:WIRED_NETADAPTERNAME = 'Ethernet' }
+      New-VMSwitch -SwitchName $env:VAGRANT_NETWORK_BRIDGE -NetAdapterName $env:WIRED_NETADAPTERNAME -AllowManagementOS $true
+    }
   }
 }
 
 # Change ExternalSwitch to Wi-Fi
 function set-wifi-vmswitch() {
-  if (-not (Test-Path env:WIFI_NETADAPTERNAME)) { $env:WIFI_NETADAPTERNAME = 'Wi-Fi' }
-  Set-VMSwitch -VMSwitch $(Get-VMSwitch -Name ExternalSwitch) -NetAdapterName $env:WIFI_NETADAPTERNAME
+  if (-not (Test-Path env:VAGRANT_NETWORK_BRIDGE)) { $env:VAGRANT_NETWORK_BRIDGE = 'ExternalSwitch' }
+  
+  if ($env:VAGRANT_NETWORK_BRIDGE -eq 'Default Switch') {
+    echo "You're using the 'Default Switch' and thus no changes can be made."
+  } else {
+    if (-not (Test-Path env:WIFI_NETADAPTERNAME)) { $env:WIFI_NETADAPTERNAME = 'Wi-Fi' }
+    Set-VMSwitch -VMSwitch $(Get-VMSwitch -Name ExternalSwitch) -NetAdapterName $env:WIFI_NETADAPTERNAME
+  }
 }
 
 # Change ExternalSwitch to Wired
 function set-wired-vmswitch() {
-  if (-not (Test-Path env:WIRED_NETADAPTERNAME)) { $env:WIRED_NETADAPTERNAME = 'Ethernet' }
-  Set-VMSwitch -VMSwitch $(Get-VMSwitch -Name ExternalSwitch) -NetAdapterName $env:WIRED_NETADAPTERNAME
+  if (-not (Test-Path env:VAGRANT_NETWORK_BRIDGE)) { $env:VAGRANT_NETWORK_BRIDGE = 'ExternalSwitch' }
+  
+  if ($env:VAGRANT_NETWORK_BRIDGE -eq 'Default Switch') {
+    echo "You're using the 'Default Switch' and thus no changes can be made."
+  } else {
+    if (-not (Test-Path env:WIRED_NETADAPTERNAME)) { $env:WIRED_NETADAPTERNAME = 'Ethernet' }
+    Set-VMSwitch -VMSwitch $(Get-VMSwitch -Name ExternalSwitch) -NetAdapterName $env:WIRED_NETADAPTERNAME
+  }
 }
