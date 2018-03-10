f735542693e54a08d15b238079dbebbc27a8bc7e
Tony Apuzzo <Tony.Apuzzo@flatironsjouve.com>
2018-03-10 08:14:46 -0700

    Split the environment and aliases so it is easier to update in the future.

---
+++ Profile.d/setVagrantAliases.ps1
@@ -0,0 +1,16 @@
+# Aliases to make Vagrant life better
+
+# Start Remote Desktop Session in the background
+function vrdp() {
+  Start-Process -NoNewWindow vagrant.exe rdp @Args
+}
+
+# Start Putty Session
+function vputty() {
+  Start-Process -NoNewWindow vagrant.exe putty @Args
+}
+
+# Reload and re-provision with Ansible
+function vputty() {
+  vagrant.exe reload --provision --provision-with=ansible_local
+}
