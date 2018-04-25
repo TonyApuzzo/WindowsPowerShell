ce0e5db9b83334da82a179dc78b5792da1ee32ca
Tony Apuzzo <tonyapuzzo@yahoo.com>
2018-04-24 21:00:09 -0600

    Add Get-GitBlobSizes

--- Profile.d/aliases.ps1
+++ Profile.d/aliases.ps1
@@ -10,3 +10,12 @@ ri alias:rmdir
 function vrdp() {
   Start-Process -NoNewWindow vagrant.exe rdp @Args
 }
+
+# Use git bash to get a listing of files by size in git
+
+function Get-GitBlobSizes() {
+  # Inspired by https://stackoverflow.com/a/42544963/2430594
+  bash -c @'
+    git rev-list --objects --all | git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' | awk '/^blob/ {print substr(\$0,6)}' | sort --numeric-sort --key=2 | cut --complement --characters=13-40 | numfmt --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+'@
+}
