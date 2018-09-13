72723129ea42ff8ac8118bd72a9981dc0f3188b2
Tony Apuzzo <Tony.Apuzzo@flatironsjouve.com>
2018-09-13 01:48:09 -0600

    Update Solarized colors based on latest fixes.

--- Profile.d/setSolarizedColors.ps1
+++ Profile.d/setSolarizedColors.ps1
@@ -1,11 +1,12 @@
 # Set My preferred theme colors
-$colorTheme = (Join-Path -Path $profileDir -ChildPath $(
+$colorTheme = (Join-Path -Path (Split-Path -Parent -Path $PROFILE) -ChildPath $(
     switch($HOST.UI.RawUI.BackgroundColor.ToString()){
-      'White'{'Set-SolarizedLightColorDefaults.ps1'}
-      'Black'{'Set-SolarizedDarkColorDefaults.ps1'}
-      default{return}
-    }
-  ))
+	    'White'{'Set-SolarizedLightColorDefaults.ps1'}
+		'Black'{'Set-SolarizedDarkColorDefaults.ps1'}
+		default{return}
+	}
+))
 if (Test-Path($colorTheme)) {
   . $colorTheme
 }
+
