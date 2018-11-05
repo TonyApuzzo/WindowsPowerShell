336c458fecc1f3ea2c599d529341cd01e97b653a
Tony Apuzzo <tonyapuzzo@yahoo.com>
2018-11-05 15:21:38 -0700

    Register both helm and kubectl

--- Profile.d/kubernetes.ps1
+++ Profile.d/kubernetes.ps1
@@ -2,13 +2,25 @@
 # See: https://github.com/tillig/ps-bash-completions
 Import-Module PSBashCompletions -ErrorAction SilentlyContinue -ErrorVariable err
 if ($err.count -eq 0) {
-  $kubectl = Get-Command kubectl -ErrorAction SilentlyContinue
-  if ($kubectl) {
-    $kubectlCompletions = Get-Item -Path "$env:TEMP\kubectl_completions.sh" -ErrorAction SilentlyContinue
-    if ($null -eq $kubectlCompletions) {
-      & $kubectl completion bash | Out-File -Encoding ASCII -NoNewline -FilePath "$env:TEMP\kubectl_completions.sh"
-      $kubectlCompletions = Get-Item -Path "$env:TEMP\kubectl_completions.sh" -ErrorAction Stop
+
+  function Get-Completions {
+    param ( [String]$Name )
+    $command = Get-Command -Name $Name -ErrorAction SilentlyContinue
+    if ($command) {
+      $commandCompletions = Get-Item -Path "$env:TEMP\${command}_completions.sh" -ErrorAction SilentlyContinue
+      if ($null -eq $commandCompletions) {
+        ((& $command completion bash) -join "`n") | Set-Content -Encoding ASCII -NoNewline -Path "${env:TEMP}\${command}_completions.sh"
+        $commandCompletions = Get-Item -Path "${env:TEMP}\${command}_completions.sh" -ErrorAction Stop
+      }
+      $commandCompletions.FullName
     }
-    Register-BashArgumentCompleter "kubectl" $kubectlCompletions.FullName
   }
+
+  foreach ($cmd in "kubectl", "helm") {
+    $script = Get-Completions $cmd
+    if ($script) {
+      Register-BashArgumentCompleter $cmd $script
+    }
+  }
+  
 }
