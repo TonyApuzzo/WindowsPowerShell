32b8879d1dbe14907dd3256eb3c532c94ff2f280
Tony Apuzzo <Tony.Apuzzo@flatironsjouve.com>
2020-02-13 16:31:24 -0700

    Don't use notepad++ as the default EDITOR since it messes up git.

--- Profile.d/setEnvironment.ps1
+++ Profile.d/setEnvironment.ps1
@@ -1,8 +1,8 @@
 # Persistently set environment variables if they are not set
 
-if (-not (Test-Path env:EDITOR)) {
+if (-not (Test-Path Env:EDITOR)) {
   # Set the environment for this invocation
-  $env:EDITOR = Get-Command notepad++.exe | Select-Object -ExpandProperty Source
+  $env:EDITOR = (Get-Command vim.exe | Select-Object -ExpandProperty Source).Replace('\\','/')
 
   # Persistently set the environment variable
   [System.Environment]::SetEnvironmentVariable('EDITOR', $env:EDITOR, [System.EnvironmentVariableTarget]::User)
